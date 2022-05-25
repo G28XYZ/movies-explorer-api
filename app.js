50f75483ed949fb040f3d11288af3aea18a9c5cd
Александр Кучеренко <g28xyz@ya.ru>
2022-05-25 11:49:38 +0700

    feat: рефакторинг

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const { PORT = 3000, DATABASE = 'moviesdb' } = process.env;
 const app = express();
 const routes = require('./routes');
 const limiter = require('./middlewares/limiter');
+const cors = require('./middlewares/cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,38 +22,7 @@ app.use(cookieParser());
 app.use(helmet());
 
 // CORS
-
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  /(https|http)?:\/\/(?:www\.|(?!www))front-movies.nomoredomains.xyz\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-
-  if (
-    allowedCors.some((e) => e.test && e.test(origin)) ||
-    allowedCors.includes(origin)
-  ) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  }
-
-  return next();
-});
+app.use(cors);
 
 mongoose.connect(`mongodb://127.0.0.1:27017/${DATABASE}`, {
   useUnifiedTopology: true,
