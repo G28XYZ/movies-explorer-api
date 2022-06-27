8d0a1ee984219cec4476782c9084a3f990c281c3
Aleksandr Kucherenko <g28xyz@ya.ru>
2022-06-27 21:14:35 +0700

    feat: create route signup

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,27 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
 const NotFoundError = require('../errors/NotFoundError');
-const ConflictError = require('../errors/ConflictError');
+// const ConflictError = require('../errors/ConflictError');
 const { errorMessages } = require('../utils/constants');
 const db = require('../db');
 
 require('dotenv').config();
 
-const { JWT_SECRET = 'JWT_SECRET', NODE_ENV } = process.env;
+// const { JWT_SECRET = 'JWT_SECRET', NODE_ENV } = process.env;
 
-const User = require('../models/user');
+// const User = require('../models/user');
 
-module.exports.getMe = (req, res, next) => {
-  const { _id } = req.user;
-  User.find({ _id })
-    .then((user) => {
-      if (!user) {
-        next(new NotFoundError(errorMessages.userNotFound));
-      }
-      return res.send(...user);
-    })
-    .catch(next);
-};
+// module.exports.getMe = (req, res, next) => {
+//   const { _id } = req.user;
+//   User.find({ _id })
+//     .then((user) => {
+//       if (!user) {
+//         next(new NotFoundError(errorMessages.userNotFound));
+//       }
+//       return res.send(...user);
+//     })
+//     .catch(next);
+// };
 
 module.exports.createUser = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -66,62 +66,62 @@ module.exports.createUser = async (req, res, next) => {
   //   });
 };
 
-module.exports.updateProfile = (req, res, next) => {
-  const { name, email } = req.body;
+// module.exports.updateProfile = (req, res, next) => {
+//   const { name, email } = req.body;
 
-  const findAndUpdate = () =>
-    User.findByIdAndUpdate(
-      req.user._id,
-      { name, email },
-      { runValidators: true }
-    );
+//   const findAndUpdate = () =>
+//     User.findByIdAndUpdate(
+//       req.user._id,
+//       { name, email },
+//       { runValidators: true }
+//     );
 
-  User.find({ email })
-    .then(([user]) => {
-      if (user && user._id.toString() !== req.user._id) {
-        throw new ConflictError(errorMessages.updateProfile);
-      }
-      return findAndUpdate();
-    })
-    .then(() => {
-      res.send({
-        name,
-        email,
-      });
-    })
-    .catch(next);
-};
+//   User.find({ email })
+//     .then(([user]) => {
+//       if (user && user._id.toString() !== req.user._id) {
+//         throw new ConflictError(errorMessages.updateProfile);
+//       }
+//       return findAndUpdate();
+//     })
+//     .then(() => {
+//       res.send({
+//         name,
+//         email,
+//       });
+//     })
+//     .catch(next);
+// };
 
-module.exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  User.findUserByCredentials(email, password)
-    .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'JWT_SECRET',
-        {
-          expiresIn: '7d',
-        }
-      );
+// module.exports.login = (req, res, next) => {
+//   const { email, password } = req.body;
+//   User.findUserByCredentials(email, password)
+//     .then((user) => {
+//       const token = jwt.sign(
+//         { _id: user._id },
+//         NODE_ENV === 'production' ? JWT_SECRET : 'JWT_SECRET',
+//         {
+//           expiresIn: '7d',
+//         }
+//       );
 
-      res.cookie('jwt', token, {
-        maxAge: 3600000,
-        httpOnly: true,
-        secure: true,
-        sameSite: 'None',
-      });
-      res.send({ token });
-    })
-    .catch(next);
-};
+//       res.cookie('jwt', token, {
+//         maxAge: 3600000,
+//         httpOnly: true,
+//         secure: true,
+//         sameSite: 'None',
+//       });
+//       res.send({ token });
+//     })
+//     .catch(next);
+// };
 
-module.exports.logout = (req, res) => {
-  res.cookie('jwt', '', {
-    maxAge: 0,
-    httpOnly: true,
-    secure: true,
-    sameSite: 'None',
-  });
-  res.clearCookie('jwt');
-  return res.send({ message: 'logout - ok!' });
-};
+// module.exports.logout = (req, res) => {
+//   res.cookie('jwt', '', {
+//     maxAge: 0,
+//     httpOnly: true,
+//     secure: true,
+//     sameSite: 'None',
+//   });
+//   res.clearCookie('jwt');
+//   return res.send({ message: 'logout - ok!' });
+// };
